270ae1fb451e086ae4cf0e2d2520a83768d5552a
Eli O <ikwost@gmail.com>
2022-10-17 23:01:05 +0300

    LocationDataSource step 5

--- src/main/kotlin/com/ikwost/domain/MapRoomController.kt
+++ src/main/kotlin/com/ikwost/domain/MapRoomController.kt
@@ -2,10 +2,20 @@ package com.ikwost.domain
 
 import com.ikwost.domain.model.Member
 import com.ikwost.domain.repository.LocationDataSource
+import io.ktor.websocket.*
 import java.util.concurrent.ConcurrentHashMap
 
 class MapRoomController(
     private val locationDataSource: LocationDataSource
 ) {
     private val members = ConcurrentHashMap<String, Member>()
+
+
+    fun onJoin(
+        username: String,
+        sessionId: String,
+        socket: WebSocketSession
+    ) {
+
+    }
 }
