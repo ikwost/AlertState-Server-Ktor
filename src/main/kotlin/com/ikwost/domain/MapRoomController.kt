dd4fabf0cfb2587f393cc5161a2a94eda2441fed
Eli O <ikwost@gmail.com>
2022-10-19 20:55:20 +0300

    LocationDataSource step 7

--- src/main/kotlin/com/ikwost/domain/MapRoomController.kt
+++ src/main/kotlin/com/ikwost/domain/MapRoomController.kt
@@ -5,6 +5,8 @@ import com.ikwost.domain.model.Member
 import com.ikwost.domain.repository.LocationDataSource
 import com.ikwost.util.MemberAlreadyExistsException
 import io.ktor.websocket.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.util.concurrent.ConcurrentHashMap
 
 class MapRoomController(
@@ -28,11 +30,29 @@ class MapRoomController(
         )
     }
 
-    fun sendLocation(userId: String, location: UserLocation) {
-        val userLocationEntity = UserLocation(
-            lat = location.lat,
-            lng = location.lng,
-            userId = userId
-        )
+    suspend fun sendLocation(userId: String, location: UserLocation) {
+
+        members.values.forEach { member ->
+            val userLocationEntity = UserLocation(
+                lat = location.lat,
+                lng = location.lng,
+                userId = userId
+            )
+            locationDataSource.insertLocation(userLocationEntity)
+
+            val parsedLocation = Json.encodeToString(userLocationEntity)
+            member.socket.send(Frame.Text(parsedLocation))
+        }
+    }
+
+    suspend fun getAllLocations(): List<UserLocation> {
+        return locationDataSource.gelAllLocations()
+    }
+
+    suspend fun tryDisconnect(username: String) {
+        members[username]?.socket?.close()
+        if (members.containsKey(username)) {
+            members.remove(username)
+        }
     }
 }
---
+++ src/main/kotlin/com/ikwost/routes/MapSocketRoute.kt
@@ -0,0 +1,70 @@
+package com.ikwost.routes
+
+import com.ikwost.data.model.UserLocation
+import com.ikwost.domain.MapRoomController
+import com.ikwost.domain.model.ApiResponse
+import com.ikwost.domain.model.Endpoint
+import com.ikwost.domain.model.UserSession
+import com.ikwost.domain.repository.UserDataSource
+import com.ikwost.util.Constants.AUTHENTICATION_PROVIDER_NAME
+import com.ikwost.util.MemberAlreadyExistsException
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.serialization.json.Json
+
+fun Route.mapSocket(
+    app: Application,
+    mapRoomController: MapRoomController,
+    userDataSource: UserDataSource
+) {
+    authenticate(AUTHENTICATION_PROVIDER_NAME) {
+        webSocket("/map-socket") {
+            val userSession = call.principal<UserSession>()
+            if (userSession == null) {
+                close()
+                app.log.info("INVALID SESSION")
+                call.respondRedirect(Endpoint.Unauthorized.path)
+            } else {
+                try {
+                    mapRoomController.onJoinMapRoom(
+                        username = userSession.name,
+                        sessionId = userSession.id,
+                        socket = this
+                    )
+                    call.respond(
+                        message = ApiResponse(
+                            success = true,
+                            user = userDataSource.getUserInfo(userId = userSession.id)
+                        ),
+                        status = HttpStatusCode.OK
+                    )
+                    incoming.consumeEach { frame ->
+                        if (frame is Frame.Text) {
+                            mapRoomController.sendLocation(
+                                userId = userSession.id,
+                                location = receiveDeserialized<UserLocation>()
+                            )
+                        }
+                    }
+
+                } catch (e: MemberAlreadyExistsException) {
+                    call.respond(HttpStatusCode.Conflict)
+                    call.respondRedirect(Endpoint.Unauthorized.path)
+                } catch (e: Exception) {
+                    app.log.info("GETTING WEBSOCKET ERROR: ${e.message}")
+                    call.respondRedirect(Endpoint.Unauthorized.path)
+                } finally {
+                    mapRoomController.tryDisconnect(userSession.name)
+                }
+            }
+
+
+        }
+    }
+}
